b87f4d00da100188a894827b1b3663010c107abf
SHALINI D <119507853+ShaliniD16@users.noreply.github.com>
2025-07-12 14:24:56 +0530

    Update SpringLearnApplication.java

--- Week4_Spring_Rest_Using_SpringBoot_3/Exercise1_Create_A_Spring_Web_Project_Using_Maven/Code/SpringLearnApplication.java
+++ Week4_Spring_Rest_Using_SpringBoot_3/Exercise1_Create_A_Spring_Web_Project_Using_Maven/Code/SpringLearnApplication.java
@@ -2,7 +2,6 @@ package com.cognizant.spring_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
